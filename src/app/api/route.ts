8c36184aecfb88d384758684acb591b6feebfb67
obrkn <k33mdukb9n@yahoo.co.jp>
2024-01-02 18:46:43 -0800

    added log emitter

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,21 @@
-export async function POST() {
+export async function POST(request: Request) {
+  console.log("POST request received");
+  console.log(request.body);
+  const data = await request.json();
+  console.log(data);
+  // if (stream === undefined || stream === null) {
+  //   return new Response("Bad Request", { status: 400 });
+  // }
+  // let reader = stream.getReader();
+
+  // let result;
+  // do {
+  //   result = await reader.read();
+  //   if (!result.done) {
+  //     // Process the chunk (result.value)
+  //     console.log(result.value);
+  //   }
+  // } while (!result.done);
+
   return new Response("OK", { status: 200 });
 }
